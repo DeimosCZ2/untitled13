763feb5770744ef75029b2253b29e713cdbb4820
DeimosC1 <gaaraofthesandfifthkazekage@gmail.com>
2021-02-17 16:28:48 +0100

    FungujeIguess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'untitled13';
+  grid: boolean[][] = [
+    [false, false, true, false],
+    [false, false, true, false],
+    [true, false, true, true],
+    [false, true, true, false],
+  ];
+
+  toggle(x: number, y: number): void {
+    this.grid[y][x] = !this.grid[y][x];
+
+    if (x - 1 >= 0) {
+      this.grid[y][x - 1] = !this.grid[y][x - 1];
+    }
+    if (x + 1 < 4) {
+      this.grid[y][x + 1] = !this.grid[y][x + 1];
+    }
+    if (y - 1 >= 0) {
+      this.grid[y - 1][x] = !this.grid[y - 1][x];
+    }
+    if (y + 1 < 4) {
+      this.grid[y + 1][x] = !this.grid[y + 1][x];
+    }
+
+  }
+
 }
